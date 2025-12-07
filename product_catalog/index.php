3b6d881b89e0ec4c397ff70faa7d8d9e2128884f
NPotts24 <najaepretty@gmail.com>
2025-12-07 15:13:29 -0500

    changing things

--- product_catalog/index.php
+++ product_catalog/index.php
@@ -1,9 +1,14 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Product Catalog</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="/product_catalog/styles.css">
 </head>
 <body>
     <h1>Our Product Catalog</h1>
