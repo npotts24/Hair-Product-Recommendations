ff4fb0893d61f670ac68f1af22f99c252c7fc746
NPotts24 <najaepretty@gmail.com>
2025-12-07 14:39:34 -0500

    updating quiz files

--- Website/Hair_Quiz/quiz.php
+++ Website/Hair_Quiz/quiz.php
@@ -1,11 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
+     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hair Quiz</title>
-
-    
     <link rel="stylesheet" href="quiz.css">
 </head>
 <body>
